fd5321edc25d2dd58c3ea22b6612b6329869a6fd
dvortsovav <dvor524588@gmail.com>
2024-12-22 11:46:01 +0300

    Add files via upload

--- лаб 2/задание 2/task2.py
+++ лаб 2/задание 2/task2.py
@@ -1,19 +1,8 @@
-BOOKS_DATABASE = [
-    {
-        "id": 1,
-        "name": "test_name_1",
-        "pages": 200,
-    },
-    {
-        "id": 2,
-        "name": "test_name_2",
-        "pages": 400,
-    }
-]
+BOOKS_DATABASE = [{"id": 1, "name": "test_name_1", "pages": 200, }, {"id": 2, "name": "test_name_2", "pages": 400, }]
 
 
 class Book:
-    def __init__(self, id_, name, pages):
+    def __init__(self, id_: int, name: str, pages: int):
         self.id = id_
         self.name = name
         self.pages = pages
@@ -22,7 +11,7 @@ class Book:
         return f'Книга "{self.name}"'
 
     def __repr__(self):
-        return f'Book(id_={self.id}, name=\'{self.name}\', pages={self.pages})'
+        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
 
 
 class Library:
@@ -48,9 +37,8 @@ if __name__ == '__main__':
     empty_library = Library()  # инициализируем пустую библиотеку
     print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
 
-    list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
-    ]
+    list_books = [Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in
+                  BOOKS_DATABASE]
     library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
